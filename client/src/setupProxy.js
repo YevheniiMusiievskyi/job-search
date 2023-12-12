8b41f4a61d450e7cdbb07ef141243afbde980b49
Yevhenii Musiievskyi (extymu01) <yevhenii.musiievskyi@hrs.com>
2023-12-13 00:03:32 +0200

    websocket path update

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        createProxyMiddleware(['/api', '/ws'], {
+        createProxyMiddleware(['/api', '/api/ws'], {
             target: process.env.back_url,
             logLevel: 'debug',
             ws: true
